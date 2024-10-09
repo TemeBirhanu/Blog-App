2a14faff1925e9ede0b8420b3dfb3bbbe3f33ac2
Temesgen <temesgenberhanu674@gmail.com>
2024-10-09 08:05:53 -0700

    fixed post

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -28,8 +28,7 @@ export const getPost = (req, res) => {
 
 // Add a new post (protected route)
 export const addPost = (req, res) => {
-  const q =
-    'INSERT INTO posts(`title`, `desc`, `img`, `cat`, `date`, `uid`) VALUES (?, ?, ?, ?, ?, ?)';
+  const q = 'INSERT INTO users(`username`,`email`,`password`) VALUES (?)';
 
   const values = [
     req.body.title,
